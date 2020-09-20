78e9c163a601a8feaf6556508307ea3325161a14
IndikaKuma <ipkumarawd@yahoo.com>
2020-09-20 23:34:52 +0200

    Updated training-db

--- lab4/training-db/resources/db_util.py
+++ lab4/training-db/resources/db_util.py
@@ -11,11 +11,11 @@ class DBUtil:
     def __init__(self):
         db_loc = os.environ['DB_LOC']
         self.engine = db.create_engine('sqlite:///' + db_loc + 'trainingdata.db', echo=True)
-        self.Base = declarative_base()
-        self.Base.metadata.reflect(self.engine)
+        self._reflect()
 
     def create_tb(self, table_name, column_names):
         conn = self.engine.connect()
+        self._reflect()
         trans = conn.begin()
         columns = (Column(name, Float, quote=False) for name in column_names)
         v_table = Table(table_name, self.Base.metadata, Column('id', Integer, primary_key=True, autoincrement=True),
@@ -26,11 +26,13 @@ class DBUtil:
     def drop_tb(self, table_name):
         conn = self.engine.connect()
         trans = conn.begin()
+        self._reflect()
         v_table = self.Base.metadata.tables[table_name]
         v_table.drop(self.engine, checkfirst=True)
         trans.commit()
 
     def add_data_records(self, table_name, records):
+        self._reflect()
         v_table = self.Base.metadata.tables[table_name]
         query = db.insert(v_table)
         connection = self.engine.connect()
@@ -39,6 +41,7 @@ class DBUtil:
         trans.commit()
 
     def read_data_records(self, table_name):
+        self._reflect()
         v_table = self.Base.metadata.tables[table_name]
         connection = self.engine.connect()
         trans = connection.begin()
@@ -46,3 +49,7 @@ class DBUtil:
         df = pd.read_sql_query(query, con=connection)
         trans.commit()
         return df
+
+    def _reflect(self):
+        self.Base = declarative_base()
+        self.Base.metadata.reflect(self.engine)
